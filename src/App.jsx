bee5fe57454eb60bfb820b2df58282697830f73b
sajid malik <sajidcee@gmail.com>
2025-04-16 11:01:26 +0500

    react project

--- src/App.jsx
+++ src/App.jsx
@@ -36,8 +36,9 @@ setConvertedAmount(amount * currencyInfo[to])
           backgroundImage: `url('https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
       }}
   >
-///////////////
-<div className="w-full md:w-1/2 flex flex-col justify-center items-center text-white p-8 bg-gradient-to-r from-black/30 to-black/25">
+
+
+<div className=" w-full max-w-lg mx-auto h-100 flex flex-col justify-center items-center text-white p-8 bg-gradient-to-r rounded-xl from-black/30 to-black/25">
   <img 
     src="./sajidimg.jpeg"  // <- your image in public folder
     alt="Sajid Malik" 
@@ -47,7 +48,7 @@ setConvertedAmount(amount * currencyInfo[to])
   <h1 className="text-4xl font-extrabold tracking-wide mb-3 text-center 
   drop-shadow-md">Sajid Malik</h1>
 
-  <p className="text-white/80 text-center text-lg max-w-md leading-relaxed tracking-wide">
+  <p className="text-white/80 text-center text-lg max-w-md leading-relaxed tracking-wide ">
     I am a <span className="text-blue-300 font-medium">web developer</span> passionate about learning new technologies.
     This <span className="text-yellow-300 font-medium">currency converter</span> is one of my React projects using live exchange rates!
   </p>
@@ -74,10 +75,9 @@ setConvertedAmount(amount * currencyInfo[to])
 
 </div>
 
-///////////////
 
-      <div className="w-full">
-          <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+      <div className="w-full max-w-2xl mx-auto border border-gray-60 rounded-xl p-8 backdrop-blur-sm bg-white/30 shadow-lg">
+          <div className="w-full max-w-2xl mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
               <form
                   onSubmit={(e) => {
                       e.preventDefault();
@@ -85,7 +85,7 @@ setConvertedAmount(amount * currencyInfo[to])
                      
                   }}
               >
-                  <div className="w-full mb-1">
+                  <div className="w-full mb-2 ">
                       <InputBox
                           label="From"
                           amount={amount}
@@ -125,6 +125,9 @@ setConvertedAmount(amount * currencyInfo[to])
               </form>
           </div>
       </div>
+
+
+
   </div> )
 }
 
